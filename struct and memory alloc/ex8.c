766b3ead2e0858443508ed556d2b603ae0105413
Islabelle V M Bastos <146977654+islabellevitoria@users.noreply.github.com>
2024-01-24 16:39:08 -0300

    Update ex8.c

--- struct and memory alloc/ex8.c
+++ struct and memory alloc/ex8.c
@@ -15,6 +15,8 @@ typedef struct{
 
 void ComparaDataNasc();
 
+//código que ordena alunos em ordem crescente de idade
+
 int main(){
 
     int n, i;
@@ -28,27 +30,27 @@ int main(){
         scanf("%d%d%d%d%[^\n]", &alunos[i].matricula, &alunos[i].datanas.dia, &alunos[i].datanas.mes, &alunos[i].datanas.ano, alunos[i].nome);
     }
 
-    ComparaDataNasc(alunos, temp, n, i);
+    ComparaDataNasc(alunos, temp, n, i); //funçao que compara a idade
 
 
     return 0;
 }
 
-void ComparaDataNasc(Dados *alunos, Dados temp, int n, int i) {
+void ComparaDataNasc(Dados *alunos, Dados temp, int n, int i) { 
     int j;
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
-            if (alunos[j].datanas.ano < alunos[j+1].datanas.ano) {
+            if (alunos[j].datanas.ano < alunos[j+1].datanas.ano) { //compara o ano
                 temp = alunos[j];
                 alunos[j] = alunos[j+1];
                 alunos[j+1] = temp;
             } else if (alunos[j].datanas.ano == alunos[j+1].datanas.ano) {
-                if (alunos[j].datanas.mes < alunos[j+1].datanas.mes) {
+                if (alunos[j].datanas.mes < alunos[j+1].datanas.mes) { //se o ano for igual, compara o mes
                     temp = alunos[j];
                     alunos[j] = alunos[j+1];
                     alunos[j+1] = temp;
                 } else if (alunos[j].datanas.mes == alunos[j+1].datanas.mes) {
-                    if (alunos[j].datanas.dia < alunos[j+1].datanas.dia) {
+                    if (alunos[j].datanas.dia < alunos[j+1].datanas.dia) { //se o ano e o mes forem iguais, compara o dia
                         temp = alunos[j];
                         alunos[j] = alunos[j+1];
                         alunos[j+1] = temp;
